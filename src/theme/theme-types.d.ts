9f546cff2eb544c11494098f1a02df437938d867
MuhammadQumailRaza112 <mqumailr@gmail.com>
2025-03-10 20:32:49 +0500

    background dark

---
+++ src/theme/theme-types.d.ts
@@ -0,0 +1,218 @@
+export declare global {}
+
+import type {} from "@mui/material/themeCssVarsAugmentation";
+
+declare module "@mui/material/styles" {
+  interface TypographyVariants {
+    "heading-1-bold": React.CSSProperties;
+    "heading-1-semiBold": React.CSSProperties;
+    "heading-1-medium": React.CSSProperties;
+    "heading-1-regular": React.CSSProperties;
+    "heading-2-bold": React.CSSProperties;
+    "heading-2-semiBold": React.CSSProperties;
+    "heading-2-medium": React.CSSProperties;
+    "heading-2-regular": React.CSSProperties;
+    "heading-3-bold": React.CSSProperties;
+    "heading-3-semiBold": React.CSSProperties;
+    "heading-3-medium": React.CSSProperties;
+    "heading-3-regular": React.CSSProperties;
+    "heading-4-bold": React.CSSProperties;
+    "heading-4-semiBold": React.CSSProperties;
+    "heading-4-medium": React.CSSProperties;
+    "heading-4-regular": React.CSSProperties;
+    "heading-5-bold": React.CSSProperties;
+    "heading-5-semiBold": React.CSSProperties;
+    "heading-5-medium": React.CSSProperties;
+    "heading-5-regular": React.CSSProperties;
+    "heading-6-bold": React.CSSProperties;
+    "heading-6-semiBold": React.CSSProperties;
+    "heading-6-medium": React.CSSProperties;
+    "heading-6-regular": React.CSSProperties;
+    "body-1-bold": React.CSSProperties;
+    "body-1-semiBold": React.CSSProperties;
+    "body-1-medium": React.CSSProperties;
+    "body-1-regular": React.CSSProperties;
+    "body-2-bold": React.CSSProperties;
+    "body-2-semiBold": React.CSSProperties;
+    "body-2-medium": React.CSSProperties;
+    "body-2-regular": React.CSSProperties;
+    "body-3-bold": React.CSSProperties;
+    "body-3-semiBold": React.CSSProperties;
+    "body-3-medium": React.CSSProperties;
+    "body-3-regular": React.CSSProperties;
+    "body-4-bold": React.CSSProperties;
+    "body-4-semiBold": React.CSSProperties;
+    "body-4-medium": React.CSSProperties;
+    "body-4-regular": React.CSSProperties;
+    "body-5-bold": React.CSSProperties;
+    "body-5-semiBold": React.CSSProperties;
+    "body-5-medium": React.CSSProperties;
+    "body-5-regular": React.CSSProperties;
+    "body-6-bold": React.CSSProperties;
+    "body-6-semiBold": React.CSSProperties;
+    "body-6-medium": React.CSSProperties;
+    "body-6-regular": React.CSSProperties;
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    "heading-1-bold": React.CSSProperties;
+    "heading-1-semiBold": React.CSSProperties;
+    "heading-1-medium": React.CSSProperties;
+    "heading-1-regular": React.CSSProperties;
+    "heading-2-bold": React.CSSProperties;
+    "heading-2-semiBold": React.CSSProperties;
+    "heading-2-medium": React.CSSProperties;
+    "heading-2-regular": React.CSSProperties;
+    "heading-3-bold": React.CSSProperties;
+    "heading-3-semiBold": React.CSSProperties;
+    "heading-3-medium": React.CSSProperties;
+    "heading-3-regular": React.CSSProperties;
+    "heading-4-bold": React.CSSProperties;
+    "heading-4-semiBold": React.CSSProperties;
+    "heading-4-medium": React.CSSProperties;
+    "heading-4-regular": React.CSSProperties;
+    "heading-5-bold": React.CSSProperties;
+    "heading-5-semiBold": React.CSSProperties;
+    "heading-5-medium": React.CSSProperties;
+    "heading-5-regular": React.CSSProperties;
+    "heading-6-bold": React.CSSProperties;
+    "heading-6-semiBold": React.CSSProperties;
+    "heading-6-medium": React.CSSProperties;
+    "heading-6-regular": React.CSSProperties;
+    "body-1-bold": React.CSSProperties;
+    "body-1-semiBold": React.CSSProperties;
+    "body-1-medium": React.CSSProperties;
+    "body-1-regular": React.CSSProperties;
+    "body-2-bold": React.CSSProperties;
+    "body-2-semiBold": React.CSSProperties;
+    "body-2-medium": React.CSSProperties;
+    "body-2-regular": React.CSSProperties;
+    "body-3-bold": React.CSSProperties;
+    "body-3-semiBold": React.CSSProperties;
+    "body-3-medium": React.CSSProperties;
+    "body-3-regular": React.CSSProperties;
+    "body-4-bold": React.CSSProperties;
+    "body-4-semiBold": React.CSSProperties;
+    "body-4-medium": React.CSSProperties;
+    "body-4-regular": React.CSSProperties;
+    "body-5-bold": React.CSSProperties;
+    "body-5-semiBold": React.CSSProperties;
+    "body-5-medium": React.CSSProperties;
+    "body-5-regular": React.CSSProperties;
+    "body-6-bold": React.CSSProperties;
+    "body-6-semiBold": React.CSSProperties;
+    "body-6-medium": React.CSSProperties;
+    "body-6-regular": React.CSSProperties;
+  }
+  interface PaletteOptions {
+    "general-1"?: { main: string };
+    "general-2"?: { main: string };
+    "general-3"?: { main: string };
+    "general-4"?: { main: string };
+    "general-5"?: { main: string };
+    "general-6"?: { main: string };
+    "general-7"?: { main: string };
+    "general-8"?: { main: string };
+    "general-9"?: { main: string };
+    "general-10"?: { main: string };
+    "general-11"?: { main: string };
+    "general-12"?: { main: string };
+    "general-13"?: { main: string };
+    "general-14"?: { main: string };
+    "general-15"?: { main: string };
+    "general-16"?: { main: string };
+    "general-17"?: { main: string };
+    "general-18"?: { main: string };
+    "general-19"?: { main: string };
+    "general-20"?: { main: string };
+    background: {
+      dark: string;
+    };
+  }
+  interface Palette {
+    "general-1"?: { main: string };
+    "general-2"?: { main: string };
+    "general-3"?: { main: string };
+    "general-4"?: { main: string };
+    "general-5"?: { main: string };
+    "general-6"?: { main: string };
+    "general-7"?: { main: string };
+    "general-8"?: { main: string };
+    "general-9"?: { main: string };
+    "general-10"?: { main: string };
+    "general-11"?: { main: string };
+    "general-12"?: { main: string };
+    "general-13"?: { main: string };
+    "general-14"?: { main: string };
+    "general-15"?: { main: string };
+    "general-16"?: { main: string };
+    "general-17"?: { main: string };
+    "general-18"?: { main: string };
+    "general-19"?: { main: string };
+    "general-20"?: { main: string };
+    background: {
+      dark: string;
+    };
+  }
+}
+
+// Update the Typography's variant prop options
+declare module "@mui/material/Typography" {
+  interface TypographyPropsVariantOverrides {
+    "heading-1-bold": true;
+    "heading-1-semiBold": true;
+    "heading-1-medium": true;
+    "heading-1-regular": true;
+    "heading-2-bold": true;
+    "heading-2-semiBold": true;
+    "heading-2-medium": true;
+    "heading-2-regular": true;
+    "heading-3-bold": true;
+    "heading-3-semiBold": true;
+    "heading-3-medium": true;
+    "heading-3-regular": true;
+    "heading-4-bold": true;
+    "heading-4-semiBold": true;
+    "heading-4-medium": true;
+    "heading-4-regular": true;
+    "heading-5-bold": true;
+    "heading-5-semiBold": true;
+    "heading-5-medium": true;
+    "heading-5-regular": true;
+    "heading-6-bold": true;
+    "heading-6-semiBold": true;
+    "heading-6-medium": true;
+    "heading-6-regular": true;
+    "body-1-bold": true;
+    "body-1-semiBold": true;
+    "body-1-medium": true;
+    "body-1-regular": true;
+    "body-2-bold": true;
+    "body-2-semiBold": true;
+    "body-2-medium": true;
+    "body-2-regular": true;
+    "body-3-bold": true;
+    "body-3-semiBold": true;
+    "body-3-medium": true;
+    "body-3-regular": true;
+    "body-4-bold": true;
+    "body-4-semiBold": true;
+    "body-4-medium": true;
+    "body-4-regular": true;
+    "body-5-bold": true;
+    "body-5-semiBold": true;
+    "body-5-medium": true;
+    "body-5-regular": true;
+    "body-6-bold": true;
+    "body-6-semiBold": true;
+    "body-6-medium": true;
+    "body-6-regular": true;
+    h1: false;
+    h2: false;
+    h3: false;
+    h4: false;
+    h5: false;
+    h6: false;
+  }
+}
